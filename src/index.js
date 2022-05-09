070b100d4c64c34c851e1fe2caf81417286cdd96
sahion <alexeysuvorov33@gmail.com>
2022-05-10 02:10:35 +0700

    init: start virtual-keyboard project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const a = document.createElement("div");
+a.classList.add("center");
+a.style.background = "red";
+a.style.width = "1000px";
+a.style.height = "1000px";
+document.body.appendChild(a);
