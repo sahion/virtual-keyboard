75a4e88c76ac830e358ab9f0b0246074024bb43c
sahion <alexeysuvorov33@gmail.com>
2022-05-10 05:09:29 +0700

    refactor: remove all files from main branch

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const a = document.createElement("div");
-a.classList.add("center");
-a.style.background = "red";
-a.style.width = "1000px";
-a.style.height = "1000px";
-document.body.appendChild(a);
